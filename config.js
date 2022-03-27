cf1d4cd9eb514effd7accb9d5a70aca889ce77c9
abdibot <abdialamanah75@gmail.com>
2022-03-27 18:42:39 +0800

    VANZZMD0

---
+++ config.js
@@ -0,0 +1,60 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': '805a6c3fa9',
+}
+
+// Other
+global.owner = ['6285824629954','6285824629954','6285824629954']
+global.pemilik = ['6285824629954']
+global.premium = ['6285824629954']
+global.pengguna = 'VANZZ OFC'
+global.botnma = 'VANZZ MD'
+global.footer = 'VANZZ OFC' //FooterText Biar Di Bawah Nya Ada Text 
+global.web = 'https://m.youtube.com/channel/UCEDb6SkiPU0theI8J_WEZZA' // Ubah Sama Lu Web Nya Bebas 
+global.web2 = 'https://github.com/abdibot' //Ubah Jga Bebas Sama lu 
+global.ganti = 'Y O U T U B E ME' //Edit Terserah Lu Mau Apa
+global.ganti2 = 'Github'
+global.ownernma = 'VANZZ OFC'
+global.packname = 'VANZZ MD'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./media/Lycho.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
